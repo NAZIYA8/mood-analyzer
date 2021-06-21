156fee7427357324f57142b6809b46f9324e4d7c
NAZIYA8 <syeda.naziya896@gmail.com>
2021-06-22 02:15:18 +0530

    Added: AnalyseMood method to return happy message or sad message

--- src/main/java/com/bridgelabz/MoodAnalyzer/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/MoodAnalyzer/MoodAnalyzer.java
@@ -1,3 +1,13 @@
+/**
+ * ****************************************************************************
+ * Purpose: This class analyzes the mood using Custom Exception.  
+ *
+ * @author Syeda Naziya
+ * @version 1.0
+ * @since 21-06-2021
+ * ****************************************************************************
+ */
+
 package com.bridgelabz.MoodAnalyzer;
 
 public class MoodAnalyzer {
@@ -8,6 +18,12 @@ public class MoodAnalyzer {
 		this.message = message;
 	}
 
+	/**
+	 * This method is used to analyze mood.
+	 * 
+	 * @return SAD if message contains sad.
+	 * @return HAPPY if message contains happy.
+	 */
 	public String analyseMood() {
 		if (message.contains("Sad"))
 			return "SAD";
---
+++ src/test/java/com/bridgelabz/MoodAnalyzer/MoodAnalyzerTest.java
@@ -0,0 +1,32 @@
+/**
+ * ****************************************************************************
+ * Purpose: This class contains the JUnit test cases to analyze the mood whether
+ * 			the mood is sad or happy  
+ *
+ * @author Syeda Naziya
+ * @version 1.0
+ * @since 21-06-2021
+ * ****************************************************************************
+ */
+
+package com.bridgelabz.MoodAnalyzer;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoodAnalyzerTest {
+
+	@Test
+	public void givenMessage_WhenSad_ShouldReturn_Sad() {
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer("This is a sad message");
+		String mood = moodAnalyzer.analyseMood();
+		Assert.assertEquals("SAD", mood);
+	}
+
+	@Test
+	public void givenMessage_WhenHappy_ShouldRetuen_Happy() {
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer("This is Happy message");
+		String mood = moodAnalyzer.analyseMood();
+		Assert.assertEquals("HAPPY", mood);
+	}
+}
